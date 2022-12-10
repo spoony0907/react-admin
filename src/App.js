e5df00d7ef9323bb25199f655370b921d3dd41c7
spoony <qingshangyoo@outlook.com>
2022-12-10 15:22:16 +0800

    lession3 react-router-dom node-sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import './App.scss';
+// 引用组件
+import Home from './views/Home';
+import About from './views/About';
+import News from './views/News';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div class="test">
+                <h1>aaaaaaaaaaa</h1>
+                <HashRouter>
+                    <Switch>
+                        <Route exact component={Home} path="/"/>
+                        <Route component={About} path="/about"/>
+                        <Route component={News} path="/news"/>
+                    </Switch>
+                </HashRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
